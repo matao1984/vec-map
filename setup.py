428bbd023a1de9f95838e7ad538cf10f49f95d5b
matao1984 <68136580+matao1984@users.noreply.github.com>
2022-05-18 15:49:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['hyperspy>=1.5','atomap>=0.2','matplotlib-scalebar','pyqt'],  # Optional
+    install_requires=['hyperspy>=1.5','atomap>=0.2','matplotlib-scalebar','pyqt5'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"

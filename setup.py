3a071d72d54a97118bd7aa6a3ae4058cc9472bc0
matao1984 <68136580+matao1984@users.noreply.github.com>
2022-07-31 08:19:46 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.1',  # Required
+    version='0.2.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -139,7 +139,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['hyperspy>=1.5','atomap>=0.2','matplotlib-scalebar','pyqt5'],  # Optional
+    install_requires=['hyperspy>=1.5','atomap>=0.2','pyqt5'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
